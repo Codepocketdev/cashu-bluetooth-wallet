0d0da299bb6d90ab11f8612abc1d1af74921dca0
Codepocketdev <artsmartin44@gmail.com>
2026-01-02 17:54:20 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,39 @@
+// Service Worker for Satoshi Pay PWA
+const CACHE_NAME = 'satoshi-pay-v1'
+const urlsToCache = [
+  '/satoshi-pay-wallet/',
+  '/satoshi-pay-wallet/index.html',
+  '/satoshi-pay-wallet/icon-192.png',
+  '/satoshi-pay-wallet/icon-512.png'
+]
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  )
+})
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  )
+})
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
